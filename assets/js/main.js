141721b24ffe598f09a13d20d6612cbbab497758
choi7858 <donguk7858@gmail.com>
2025-05-12 13:45:20 +0900

    Initial site with header/footer

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Load header
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+  // Load footer
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
